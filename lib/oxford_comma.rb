0dbac9c550e04590199654242ed861da412d50a9
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-10 19:51:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
 def oxford_comma(array)
-  if array.length == 2
-    array.join(" and ")
-  else array.length == 3
-    array[0].join(",")
-    array[1].join(",")
-    array[2].join(" and ")
-  end
+  array.length < 1 array.join
 end
